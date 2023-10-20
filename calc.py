52941eacde338ff4e3e2c9ec1a82ae15c7f2b65f
Brendan <brendanjlee5@gmail.com>
2023-10-20 10:38:02 +0900

    Backend

--- calc.py
+++ calc.py
@@ -1,9 +1,17 @@
-def calculator():
+def calculatorRunner():
     stack = []
     operators = ['+', '-', '/', '*']
 
     while (True):
         op = input('Enter Value: ')
+        
+        if (op == 'p'):
+            print(stack)
+            continue
+        if (op == 'c'):
+            stack = []
+            continue
+
         op = op.strip()
 
         # is number
@@ -15,6 +23,20 @@ def calculator():
             if not stack: 
                 stack.append(op)
                 continue
+
+            top = stack[-1]
+            
+            # top of stack is an operator -> 
+            # calculate value and add back into stack
+            if top in operators:
+                operator = stack.pop()
+                b = stack.pop()
+                res = calculate(op, b, operator)
+                print(res)
+                stack.append(res)
+            else:
+                print('not a valid operation')
+
         # operator
         else:
             # stack must not be empty
@@ -26,12 +48,24 @@ def calculator():
             if op not in operators:
                 print('not a valid operator. enter a valid operator')
                 continue
-            
-            top = stack[0]
+
+            top = stack[-1]
             
             # top of stack is a number
-            if top.isnumeric():
+            if isinstance(top, int) or isinstance(top, float):
                 stack.append(op)
             else:
                 print('not a valid operation')
 
+def calculate(a, b, operand):
+    if operand == '+':
+        res =  a + b
+    elif operand == '-':
+        res = b - a
+    elif operand == '*':
+        res = a * b
+    else:
+        res = b / a
+    return round(res, 2)
+
+calculatorRunner()
--- script.js
+++ script.js
@@ -1,5 +1,79 @@
-function calculator() {
-    stack = [];
+function CalculatorController() {
+    const stack = [];
+    const operators = ['+', '-', '*', '/'];
 
-    operators = []
+    const calculate = (a, b, op) => {
+        let res;
+        if (op === '+') res = a + b;
+        else if (op == '-') res = b - a;
+        else if (op == '*') res = a * b;
+        else res = b /a;
+
+        return parseFloat(res.toFixed(4));
+    }
+
+    const getStack = () => stack;
+
+    const runner = (input) => {
+        let op = input;
+        if (op === 'p') {
+            console.log(stack);
+            return;
+        }
+        if (op == 'c') {
+            stack.length = 0;
+            return;
+        }
+
+        // op is a number
+        if (!isNaN(op)) {
+            // convert to a float
+            op = parseFloat(op);
+
+            // add to stack if not empty
+            if (stack.length == 0) {
+                stack.push(op);
+                return;
+            }
+
+            let top = stack[stack.length - 1];
+
+            // top of stack is an operator
+            if (operators.includes(top)) {
+                let operator = stack.pop();
+                let b = stack.pop();
+                let res = calculate(op, b, operator);
+                console.log(res);
+                stack.push(res);
+            } else {
+                console.log('not a valid operation');
+            }
+
+        }
+        // operator
+        else {
+            if (stack.length == 0) {
+                console.log('stack empty. enter a number');
+                return;
+            };
+            if (!operators.includes(op)) {
+                console.log('not a valid operator. enter a valid operator');
+                return;
+            };
+
+            let top = stack[stack.length - 1];
+
+            // top of stack is a number
+            if (!isNaN(top)) stack.push(op);
+            else console.log('not a valid operation');
+        }
+    }
+}
+
+//CalculatorController();
+
+while (true) {
+    let op = prompt('Enter Value');
+    op = op.trim();
+    CalculatorController.runner(op);
 }
